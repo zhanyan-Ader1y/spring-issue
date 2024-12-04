cbd71ddd6f8fdaa567a55ed5788d9b0a91463782
zhanyan <zhanyan.work@outlook.com>
2024-12-04 17:29:48 +0800

    init

---
+++ src/test/java/com/ader1y/springissue33970/SpringIssue33970ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ader1y.springissue33970;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIssue33970ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
